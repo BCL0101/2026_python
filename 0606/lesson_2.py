b9f8bbba83ec1922a3db4f92b68eddc8a44b3178
Bernard <rx921606@gmail.com>
2026-06-06 11:27:19 +0800

    0606

--- 0606/lesson_2.py
+++ 0606/lesson_2.py
@@ -1,9 +1,9 @@
-# n = 0    # n在這是全域變數
+n = 0    # n放在這是全域變數
+
 def main():   # 自定義function
-    n = 10   # n在這裡是區域變數
+    #n = 10   # n放在這裡是區域變數
     print("這裡是main function的命名空間")   #function的程式區塊是命名空間
-    print(n)   # n在這是文件變數
+    print(n)   # n放在這是文件變數
     
 if __name__ == '__main__':
-    main()   # 整個專案的執行起點
-    print(n)
+    main()   # main()是整個專案的執行起點
